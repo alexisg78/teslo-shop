a714dabfdf215c51122dfeda8c71f5ab07aa0a88
Alexis Gonzalez <galexis.isi78@gmail.com>
2025-12-14 20:47:42 -0300

    Crear Usuario + Password encriptado

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,53 @@
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+import * as bcrypt  from 'bcrypt'
+
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+
+
+@Injectable()
+export class AuthService {
+  
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ){}
+
+  async create(createUserDto: CreateUserDto) {
+   
+    try {
+      
+      const { password, ...userData } = createUserDto
+      
+      const user = this.userRepository.create( {
+        ...userData,
+        password: bcrypt.hashSync( password, 10 )
+      } );
+
+      const saveUser= await this.userRepository.save( user ) ;
+
+      const { password:_ , ...restUser } = saveUser
+
+      return restUser;
+      // TODO: Retornar JWK de acceso
+
+    } catch (error) {
+      this.handleDbErrors( error );
+    }
+
+  }
+
+  private handleDbErrors( error: any ): never {
+    if ( error.code === '23505' )
+      throw new BadRequestException( error.detail );
+
+    console.log(error);
+
+    throw new InternalServerErrorException('Please check server logs')
+
+  }
+
+}
