78a9185c33608d616714be1da65c76fab3cf633a
Alexis Gonzalez <galexis.isi78@gmail.com>
2025-12-27 16:35:42 -0300

    Autenticación + Libreria Passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,45 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
-    TypeOrmModule.forFeature([ User ])
+    TypeOrmModule.forFeature([ User ]),
+
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+
+    JwtModule.registerAsync({
+      imports: [ ConfigModule ],
+      inject: [ ConfigService ],
+      useFactory: ( configService: ConfigService ) => {
+        
+        // console.log('Este es mi JWT_SECRET', configService.get('JWT_SECRET') )
+        // console.log('Este es mi JWT_SECRET', process.env.JWT_SECRET)
+        
+        return {
+            secret: configService.get('JWT_SECRET'),
+            signOptions: {
+            expiresIn: '2h'
+          }    
+        }
+      }
+    })
+
+    // JwtModule.register({
+    //   secret: process.env.JWT_SECRET,
+    //   signOptions: {
+    //     expiresIn: '2h'
+    //   }
+    // })
+
   ],
   exports: [ TypeOrmModule ]
 })
