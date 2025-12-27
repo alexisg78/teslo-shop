908efb7a4a99949e58f7d24fb36c5d8e11100bd7
Alexis Gonzalez <galexis.isi78@gmail.com>
2025-12-27 15:04:37 -0300

    Implementación de Login Usuario

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
 import { Repository } from 'typeorm';
@@ -6,6 +6,7 @@ import * as bcrypt  from 'bcrypt'
 
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto.js';
 
 
 @Injectable()
@@ -38,6 +39,25 @@ export class AuthService {
       this.handleDbErrors( error );
     }
 
+  }
+
+  async login( loginUserDto: LoginUserDto ){
+
+    const { email, password } = loginUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true }
+    });
+
+    if ( !user )
+      throw new UnauthorizedException('Credentials are not valid (email)')
+
+    if ( !bcrypt.compareSync( password, user.password) )
+      throw new UnauthorizedException('Credentials are not valid (password)')
+
+    return user;
+    // TODO: retornar el jwt
   }
 
   private handleDbErrors( error: any ): never {
