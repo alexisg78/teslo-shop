b31b9e3366424eb1966b7c5efc04f14649b57a13
Alexis Gonzalez <galexis.isi78@gmail.com>
2025-12-28 23:37:40 -0300

    Autenticación basada en JWT - Patron de diseño Strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,16 +2,19 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from './entities/user.entity';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   imports: [
+    ConfigModule,
+
     TypeOrmModule.forFeature([ User ]),
 
     PassportModule.register({ defaultStrategy: 'jwt' }),
@@ -25,10 +28,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         // console.log('Este es mi JWT_SECRET', process.env.JWT_SECRET)
         
         return {
-            secret: configService.get('JWT_SECRET'),
-            signOptions: {
-            expiresIn: '2h'
-          }    
+          secret: configService.get('JWT_SECRET'),
+          signOptions: { expiresIn: '2h' }    
         }
       }
     })
@@ -41,6 +42,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     // })
 
   ],
-  exports: [ TypeOrmModule ]
+  exports: [ TypeOrmModule, JwtStrategy, PassportModule, JwtModule ]
 })
 export class AuthModule {}
---
+++ src/auth/interfaces/jwt-payload.interface.ts
@@ -0,0 +1,8 @@
+
+
+
+export interface JwtPayload {
+  email: string;
+
+  // TODO: Añadir todo lo que se quiera grabar
+}
