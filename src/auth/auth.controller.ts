e2a53d11cd457715c5612ec6ed55beb99d89c6cc
Alexis Gonzalez <galexis.isi78@gmail.com>
2025-12-29 23:37:21 -0300

    Decorador custom de propiedad - GetUser

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
-import { AuthService } from './auth.service';
-
-import { CreateUserDto, LoginUserDto } from './dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+
+import { AuthService } from './auth.service';
+import { CreateUserDto, LoginUserDto } from './dto';
+
+import { GetUser } from './decorators/get-user.decorator';
+import { User } from './entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -18,13 +21,19 @@ export class AuthController {
     return this.authService.login(loginUserDto);
   }
 
-  @UseGuards( AuthGuard() )
   @Get('private')
-  testingPrivateRoute(){
+  @UseGuards( AuthGuard() )
+  testingPrivateRoute( 
+    // @Req() request: Express.Request 
+    @GetUser() user: User
+  ){
 
+    // console.log({ request: request.user })
+    
     return {
       ok: true,
-      message: 'Hola mundo Private'
+      message: 'Hola mundo Private',
+      user,
     }
   }
 
