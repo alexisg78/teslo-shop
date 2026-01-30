0ee6442e348ae3187809399fb113cf75de30024f
Alexis Gonzalez <galexis.isi78@gmail.com>
2026-01-29 21:49:59 -0300

    Clientes conectados

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,20 +1,28 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 import { MessagesWsService } from './messages-ws.service';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   
+  @WebSocketServer() wss: Socket;
+
   constructor(
     private readonly messagesWsService: MessagesWsService
   ) {}
 
   handleConnection(client: Socket) {
-    console.log('Cliente conectado: ', client.id)
+    // console.log('Cliente conectado: ', client.id)
+    this.messagesWsService.registerClient( client );
+    
+    this.wss.emit('clients-updated', this.messagesWsService.conectedClient() );
   }
 
   handleDisconnect(client: Socket) {
-    console.log('Cliente desconectado: ', client.id)
+    // console.log('Cliente desconectado: ', client.id)
+    this.messagesWsService.removeClient(client.id);
+    
+    this.wss.emit('clients-updated', this.messagesWsService.conectedClient() );
   }
 
 }
